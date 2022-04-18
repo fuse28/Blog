475c62e30e708a07239efe14278974f9bac47d9e
fuse28 <mixfuse92@gmail.com>
2022-04-18 17:46:12 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [description, setDescription] = useState("");
+  const [blog, setBlog] = useState([]);
+
+  function addBlog() {
+    setBlog([...blog, input, description]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <h1>ADD BLOG</h1>
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          className="title"
+          placeholder="Title"
+        ></input>
+        <input
+          className="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          placeholder="description"
+        ></input>
+        <button className="btn" onClick={addBlog}>
+          Add Blog
+        </button>
+      </div>
+      <div className="blogs">
+        <div className="eachBlog">
+          <h3>hello</h3>
+          <button className="delete-btn">Delete</button>
+          <button className="edit-btn">Edit</button>
+        </div>
+      </div>
     </div>
   );
 }
