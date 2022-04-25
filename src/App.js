2739d2366ef0c40b81bc04a6b3a5876d69f645aa
fuse28 <mixfuse92@gmail.com>
2022-04-25 10:01:45 +0530

    added EDIT funtion but still making it work

--- src/App.js
+++ src/App.js
@@ -7,25 +7,59 @@ function App() {
   const [input, setInput] = useState("");
   const [description, setDescription] = useState("");
   const [blog, setBlog] = useState([]);
+  const [toggleSubmit, setToggleSubmit] = useState(true);
+  const [edititem, setEditItem] = useState(null);
 
   console.log(input);
   console.log(description);
 
   function addBlog() {
     if (!input) {
-    } else {
-      setBlog([...blog, input, description]);
+      alert("insert data");
+    } else if (input || (description && !toggleSubmit)) {
+      setBlog(
+        blog.map((ele) => {
+          if (ele.id == edititem) {
+            return { ...ele, name: input, description };
+          }
+          return ele;
+        })
+      );
+      setToggleSubmit(true);
+
       setInput("");
       setDescription("");
+      setEditItem(null);
+    } else {
+      const allInputData = {
+        id: new Date().getTime().toString(),
+        name: input,
+        description,
+      };
+      setBlog([...blog, allInputData]);
+      setInput("");
+      setDescription("");
+      console.log(allInputData);
     }
   }
-  function deleteBlog(id) {
-    console.log(id);
-    const delBLog = blog.filter((ele, ind) => {
-      return ind !== id;
+
+  function deleteBlog(index) {
+    const delBLog = blog.filter((ele) => {
+      return index != ele.id;
     });
     setBlog(delBLog);
   }
+  const editBlog = (id) => {
+    let editBlog = blog.find((ele) => {
+      return (ele.id = id);
+    });
+    console.log(editBlog);
+    setToggleSubmit(true);
+
+    setInput(blog.name);
+    setDescription(blog.name);
+    setEditItem(id);
+  };
 
   return (
     <div className="app">
@@ -43,19 +77,28 @@ function App() {
           onChange={(e) => setDescription(e.target.value)}
           placeholder="description"
         ></input>
-        <button className="btn" onClick={addBlog}>
-          Add Blog
-        </button>
+
+        {toggleSubmit ? (
+          <button className="btn" onClick={addBlog}>
+            Add Blog
+          </button>
+        ) : (
+          <button className="btn" onClick={addBlog}>
+            Edit Blog
+          </button>
+        )}
       </div>
       <div className="blogs">
-        {blog.map((ele, ind) => {
+        {blog.map((ele) => {
           return (
-            <div className="eachBlog" key={ind}>
-              <h3>{ele}</h3>
-              <button className="delete-btn" onClick={() => deleteBlog(ind)}>
+            <div className="eachBlog" key={ele.id}>
+              <h3>{ele.name}</h3>
+              <button className="delete-btn" onClick={() => deleteBlog(ele.id)}>
                 Delete
               </button>
-              <button className="edit-btn">Edit</button>
+              <button className="edit-btn" onClick={() => editBlog(ele.id)}>
+                Edit
+              </button>
             </div>
           );
         })}
